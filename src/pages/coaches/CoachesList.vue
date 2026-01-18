2d268e1466a5f664e1461aa3e013a42cf180e427
PjMondelaers <pieterjan.dev@gmail.com>
2026-01-18 21:23:42 +0100

    feat: add conditional rendering for coaches list and no coaches message

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,11 +6,12 @@
       <button>Vernieuwen</button>
       <router-link to="/register">Registreer als Coach</router-link>
     </div>
-    <ul>
+    <ul v-if="hasCoaches">
       <li v-for="coach in filteredCoaches" :key="coach.id">
         {{ coach.firstName }}
       </li>
     </ul>
+    <h3 v-else>Geen coaches gevonden</h3>
   </section>
 </template>
 
@@ -19,6 +20,9 @@ export default {
   computed: {
     filteredCoaches() {
       return this.$store.getters["coaches/coaches"];
+    },
+    hasCoaches() {
+      return this.$store.getters["coaches/hasCoaches"];
     },
   },
 };
