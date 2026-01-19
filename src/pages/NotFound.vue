4524ecfb0c32090b699444f247815d234e12db5b
PjMondelaers <pieterjan.dev@gmail.com>
2026-01-19 21:52:28 +0100

    feat: update NotFound page structure with base-card for improved user experience

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,5 +1,13 @@
 <template>
-  <div></div>
+  <section>
+    <base-card>
+      <h2>Pagina niet gevonden</h2>
+      <p>
+        Deze pagina kon niet gevonden worden - misschien wil je onze
+        <router-link to="/coaches">coaches</router-link> bekijken
+      </p>
+    </base-card>
+  </section>
 </template>
 
 <script>
