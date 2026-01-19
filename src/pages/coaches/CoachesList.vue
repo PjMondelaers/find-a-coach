7c46c7060f2df03957ee31d9fdabc3231a9113da
PjMondelaers <pieterjan.dev@gmail.com>
2026-01-19 11:34:05 +0100

    feat: create CoachItem component and integrate it into CoachesList for improved coach display

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -7,16 +7,27 @@
       <router-link to="/register">Registreer als Coach</router-link>
     </div>
     <ul v-if="hasCoaches">
-      <li v-for="coach in filteredCoaches" :key="coach.id">
-        {{ coach.firstName }}
-      </li>
+      <coach-item
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :first-name="coach.firstName"
+        :last-name="coach.lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas"
+      ></coach-item>
     </ul>
     <h3 v-else>Geen coaches gevonden</h3>
   </section>
 </template>
 
 <script>
+import CoachItem from "../../components/coaches/CoachItem.vue";
 export default {
+  components: {
+    CoachItem,
+  },
+
   computed: {
     filteredCoaches() {
       return this.$store.getters["coaches/coaches"];
@@ -28,4 +39,15 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
