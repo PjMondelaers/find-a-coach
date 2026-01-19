9eec459d0d83b4f9afa0900dc36f5fbe26d99b7f
PjMondelaers <pieterjan.dev@gmail.com>
2026-01-19 20:26:55 +0100

    feat: add BaseSpinner component and integrate loading state in CoachesList

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -9,11 +9,14 @@
         <base-button mode="outline" @click="loadCoaches"
           >Vernieuwen</base-button
         >
-        <base-button v-if="!isCoach" to="/register" link
+        <base-button v-if="!isCoach && !isLoading" to="/register" link
           >Registreer als Coach</base-button
         >
       </div>
-      <ul v-if="hasCoaches">
+      <div v-if="isLoading">
+        <base-spinner></base-spinner>
+      </div>
+      <ul v-else-if="hasCoaches">
         <coach-item
           v-for="coach in filteredCoaches"
           :key="coach.id"
@@ -41,6 +44,7 @@ export default {
 
   data() {
     return {
+      isLoading: false,
       activeFilters: {
         frontend: true,
         backend: true,
@@ -69,7 +73,7 @@ export default {
       });
     },
     hasCoaches() {
-      return this.$store.getters["coaches/hasCoaches"];
+      return !this.isLoading && this.$store.getters["coaches/hasCoaches"];
     },
     isCoach() {
       return this.$store.getters["coaches/isCoach"];
@@ -80,8 +84,10 @@ export default {
     setFilters(updatedFilters) {
       this.activeFilters = updatedFilters;
     },
-    loadCoaches() {
-      return this.$store.dispatch("coaches/loadCoaches");
+    async loadCoaches() {
+      this.isLoading = true;
+      await this.$store.dispatch("coaches/loadCoaches");
+      this.isLoading = false;
     },
   },
 
