68f77f8da5b76fe784bc436983c7d13352d6a2ba
PjMondelaers <pieterjan.dev@gmail.com>
2026-01-20 15:09:48 +0100

    feat(auth): add token retrieval in getters and actions for authenticated requests

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -9,8 +9,10 @@ export default {
       areas: data.areas,
     };
 
+    const token = context.rootGetters.token;
+
     const response = await fetch(
-      `https://find-a-coach-7a72f-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,
+      `https://find-a-coach-7a72f-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json?auth=${token}`,
       {
         method: "PUT",
         body: JSON.stringify(coachData),
