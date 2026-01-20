be67a6bfcb1fd7c3eb5693fade84835e1962eebb
PjMondelaers <pieterjan.dev@gmail.com>
2026-01-20 13:47:36 +0100

    feat: implement user authentication module with UserAuth component and Vuex store integration

---
+++ src/store/modules/auth/index.js
@@ -0,0 +1,15 @@
+import mutations from "./mutations";
+import getters from "./getters";
+import actions from "./actions";
+
+export default {
+  state() {
+    return {
+      userId: "c3",
+    };
+  },
+
+  mutations,
+  actions,
+  getters,
+};
